52f429c9d59ad9335eeffd0daadeade395606a21
Axel Schenkelman <axel.schenkelman@gmail.com>
2020-04-29 13:02:19 -0300

    feat(events): add BottomTabPressed event

--- src/index.test.ts
+++ src/index.test.ts
@@ -25,6 +25,7 @@ import {
   useNavigationModalDismiss,
   useNavigationScreenPop,
   useNavigationBottomTabSelect,
+  useNavigationBottomTabPress,
   useNavigationBottomTabLongPress,
   useNavigationButtonPress,
   useNavigationSearchBarUpdate,
@@ -759,6 +760,68 @@ describe('useNavigationBottomTabSelect', () => {
     triggerEvent(event1)
 
     const event2 = { selectedTabIndex: 1, unselectedTabIndex: 2 }
+    triggerEvent(event2)
+
+    expect(mockHandler).toBeCalledTimes(2)
+    expect(mockHandler).toHaveBeenNthCalledWith(1, event1)
+    expect(mockHandler).toHaveBeenNthCalledWith(2, event2)
+
+    expect(result.current).toBeUndefined()
+    expect(result.error).toBeUndefined()
+  })
+})
+
+describe('useNavigationBottomTabPress', () => {
+  let triggerEvent: (event: BottomTabLongPressedEvent) => void
+  let mockRemoveSubscription: () => void
+  let mockHandler: () => void
+
+  beforeEach(() => {
+    mockHandler = jest.fn(() => {})
+    mockRemoveSubscription = jest.fn()
+
+    Navigation.events = jest.fn().mockReturnValue({
+      registerBottomTabPressedListener: jest.fn(callback => {
+        triggerEvent = callback
+
+        return { remove: mockRemoveSubscription }
+      }),
+    })
+  })
+
+  it('should remove the event listener on unmount', () => {
+    const { result, unmount } = renderHook(() => {
+      useNavigationBottomTabPress(() => {})
+    })
+
+    unmount()
+
+    expect(mockRemoveSubscription).toBeCalledTimes(1)
+
+    expect(result.current).toBeUndefined()
+    expect(result.error).toBeUndefined()
+  })
+
+  it('should never call the handler if no event was triggered', () => {
+    const { result } = renderHook(() => {
+      useNavigationBottomTabPress(() => {})
+    })
+
+    expect(mockHandler).toBeCalledTimes(0)
+
+    expect(result.current).toBeUndefined()
+    expect(result.error).toBeUndefined()
+  })
+
+  it('should call handler twice', () => {
+    const { result } = renderHook(() => {
+      useNavigationBottomTabPress(mockHandler)
+    })
+
+    const event1 = { selectedTabIndex: 1 }
+    triggerEvent(event1)
+
+    const event2 = { selectedTabIndex: 1 }
     triggerEvent(event2)
 
     expect(mockHandler).toBeCalledTimes(2)
