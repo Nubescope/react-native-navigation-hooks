7d05ebc4215cf16852a404fc8c0216c9a48a418c
Axel Schenkelman <axel.schenkelman@gmail.com>
2020-05-12 09:19:40 -0300

    chore(comment): remove unnecesary comment

--- example/src/App.js
+++ example/src/App.js
@@ -75,32 +75,3 @@ export const startApp = () => {
     },
   })
 }
-
-// import { Navigation } from 'react-native-navigation'
-// import App from './screens/RootScreen'
-
-// /**
-//  * Register Components
-//  */
-
-// Navigation.registerComponent('com.RNNHooksSample.MainScreen', () => App)
-
-// /**
-//  * Register AppLaunched
-//  */
-
-// Navigation.events().registerAppLaunchedListener(() => {
-//   Navigation.setRoot({
-//     root: {
-//       stack: {
-//         children: [
-//           {
-//             component: {
-//               name: 'com.RNNHooksSample.MainScreen',
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   })
-// })
