4c18ef3031f83c1e25e24af0fa9026f4601561d4
Juan Pablo Garcia <juanpablogarcia@gmail.com>
2020-08-09 01:31:47 -0300

    chore(deps): improve project config - upgrade packages - remove unnecessary packages - improve eslint config - improve jest config

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-  env: {
-    test: {
-      plugins: ['dynamic-import-node'],
-    },
-    production: {
-      plugins: ['@babel/plugin-syntax-dynamic-import'],
-    },
-  },
+  presets: ['module:metro-react-native-babel-preset'],
 }
